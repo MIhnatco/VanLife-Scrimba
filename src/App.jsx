8a5d5c4b258c19dc06c1207c27e47e18cbc09de2
Ihliana <Ihnatco@protonmail.com>
2025-03-19 15:30:17 +0100

    Initial page layout - Home and About with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="h-28 flex items-center py-2.5 px-8">
+        <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
