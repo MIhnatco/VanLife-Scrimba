b77e5973eec6e25a7aa9f7ba86dbb3384bd6b42b
Ihliana <Ihnatco@protonmail.com>
2025-04-28 10:03:26 +0200

    v0.17.3 - Error handling for downloading Vans

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -11,6 +11,7 @@ function Vans() {
   const [vans, setVans] = React.useState([]);
 
   const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
   const [searchParams, setSearchParams] = useSearchParams();
   const typeFilter = searchParams.get("type");
@@ -24,107 +25,119 @@ function Vans() {
   React.useEffect(() => {
     async function loadVans() {
       setLoading(true);
-      const data = await getVans();
-      setVans(data);
-      setLoading(false);
+
+      try {
+        const data = await getVans();
+        setVans(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
     }
 
     loadVans();
   }, []);
-
-  const displayedVans = typeFilter
-    ? vans.filter((van) => van.type === typeFilter)
-    : vans;
-
-  const vanElements = displayedVans.map((van) => (
-    <div key={van.id} className="text-[#161616] my-8">
-      <Link
-        to={van.id}
-        aria-label={`View details for ${van.name}, 
-            priced at $${van.price} per day`}
-        state={{ search: searchParams.toString(), type: typeFilter }}
-      >
-        <img
-          src={van.imageUrl}
-          className="max-w-full rounded-xl"
-          alt={`Image of ${van.name}`}
-        />
-        <div className="van-info">
-          <h3 className="text-3xl font-bold my-8">{van.name}</h3>
-          <p className="text-xl font-medium my-4">
-            ${van.price}
-            <span>/day</span>
-          </p>
-        </div>
-        <i
-          className={`h-9 py-1.5 px-7 rounded-md transition duration-200 ease-[cubic-bezier(0.4,0.05,0.2,1)] text-[#ffead0] cursor-pointer ${
-            van.type
-          } ${bgColors[van.type]} selected`}
-        >
-          {van.type}
-        </i>
-      </Link>
-    </div>
-  ));
 
   //Filtering URL search parameters
   const onFilterChange = (key, value) => {
     handleFilterChange(key, value, setSearchParams);
   };
 
-  if (loading) {
-    return <Spinner />;
-  }
+  return loading ? (
+    <Spinner />
+  ) : error ? (
+    <h1>There was an error: {error.message}</h1>
+  ) : (
+    (() => {
+      const displayedVans = typeFilter
+        ? (vans || []).filter((van) => van.type === typeFilter)
+        : vans || [];
 
-  return (
-    <article className="px-6 my-5">
-      <h1 className="text-4xl font-extrabold my-4">Explore our van options</h1>
-
-      <div>
-        <button
-          className={`py-1.5 px-6 font-medium rounded-md transition-all duration-200 ease-in-out mr-2 ${
-            typeFilter === "simple"
-              ? "bg-[#E17654] text-[#ffead0]"
-              : "bg-[#ffead0] text-[#4d4d4d] hover:bg-[#E17654] hover:text-[#ffead0]"
-          }`}
-          onClick={() => onFilterChange("type", "simple")}
-        >
-          Simple
-        </button>
-        <button
-          className={`py-1.5 px-6 font-medium rounded-md transition-all duration-200 ease-in-out mr-2 ${
-            typeFilter === "rugged"
-              ? "bg-[#115e59] text-[#ffead0]"
-              : "bg-[#ffead0] text-[#4d4d4d] hover:bg-[#115e59] hover:text-[#ffead0]"
-          }`}
-          onClick={() => onFilterChange("type", "rugged")}
-        >
-          Rugged
-        </button>
-        <button
-          className={`py-1.5 px-6 font-medium rounded-md transition-all duration-200 ease-in-out mr-2 ${
-            typeFilter === "luxury"
-              ? "bg-[#161616] text-[#ffead0]"
-              : "bg-[#ffead0] text-[#4d4d4d] hover:bg-[#161616] hover:text-[#ffead0]"
-          }`}
-          onClick={() => onFilterChange("type", "luxury")}
-        >
-          Luxury
-        </button>
-        {typeFilter ? (
-          <button
-            className={`py-1.5 px-6 font-medium rounded-md bg-[#ffead0] text-[#4d4d4d] transition-all duration-200 ease-in-out hover:text-[#ffead0] hover:bg-[#f00] mr-2`}
-            onClick={() => onFilterChange("type", null)}
+      const vanElements = displayedVans.map((van) => (
+        <div key={van.id} className="text-[#161616] my-8">
+          <Link
+            to={van.id}
+            aria-label={`View details for ${van.name}, 
+                priced at $${van.price} per day`}
+            state={{ search: searchParams.toString(), type: typeFilter }}
           >
-            Clear filter
-          </button>
-        ) : null}
-      </div>
+            <img
+              src={van.imageUrl}
+              className="max-w-full rounded-xl"
+              alt={`Image of ${van.name}`}
+            />
+            <div className="van-info">
+              <h3 className="text-3xl font-bold my-8">{van.name}</h3>
+              <p className="text-xl font-medium my-4">
+                ${van.price}
+                <span>/day</span>
+              </p>
+            </div>
+            <i
+              className={`h-9 py-1.5 px-7 rounded-md transition duration-200 ease-[cubic-bezier(0.4,0.05,0.2,1)] text-[#ffead0] cursor-pointer ${
+                van.type
+              } ${bgColors[van.type]} selected`}
+            >
+              {van.type}
+            </i>
+          </Link>
+        </div>
+      ));
 
-      <section className="grid grid-cols-2 justify-center gap-8 mt-14">
-        {vanElements}
-      </section>
-    </article>
+      return (
+        <article className="px-6 my-5">
+          <h1 className="text-4xl font-extrabold my-4">
+            Explore our van options
+          </h1>
+
+          <div>
+            <button
+              className={`py-1.5 px-6 font-medium rounded-md transition-all duration-200 ease-in-out mr-2 ${
+                typeFilter === "simple"
+                  ? "bg-[#E17654] text-[#ffead0]"
+                  : "bg-[#ffead0] text-[#4d4d4d] hover:bg-[#E17654] hover:text-[#ffead0]"
+              }`}
+              onClick={() => onFilterChange("type", "simple")}
+            >
+              Simple
+            </button>
+            <button
+              className={`py-1.5 px-6 font-medium rounded-md transition-all duration-200 ease-in-out mr-2 ${
+                typeFilter === "rugged"
+                  ? "bg-[#115e59] text-[#ffead0]"
+                  : "bg-[#ffead0] text-[#4d4d4d] hover:bg-[#115e59] hover:text-[#ffead0]"
+              }`}
+              onClick={() => onFilterChange("type", "rugged")}
+            >
+              Rugged
+            </button>
+            <button
+              className={`py-1.5 px-6 font-medium rounded-md transition-all duration-200 ease-in-out mr-2 ${
+                typeFilter === "luxury"
+                  ? "bg-[#161616] text-[#ffead0]"
+                  : "bg-[#ffead0] text-[#4d4d4d] hover:bg-[#161616] hover:text-[#ffead0]"
+              }`}
+              onClick={() => onFilterChange("type", "luxury")}
+            >
+              Luxury
+            </button>
+            {typeFilter ? (
+              <button
+                className={`py-1.5 px-6 font-medium rounded-md bg-[#ffead0] text-[#4d4d4d] transition-all duration-200 ease-in-out hover:text-[#ffead0] hover:bg-[#f00] mr-2`}
+                onClick={() => onFilterChange("type", null)}
+              >
+                Clear filter
+              </button>
+            ) : null}
+          </div>
+
+          <section className="grid grid-cols-2 justify-center gap-8 mt-14">
+            {vanElements}
+          </section>
+        </article>
+      );
+    })()
   );
 }
 
